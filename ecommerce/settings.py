cef962913caa33e10a0422a8c844e7871fe0f12b
Nakka Anantha Lakshmi <ananthalakshminakka157@gmail.com>
2025-09-23 22:56:52 +0530

    Configure static files with WhiteNoise

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ecommerceapp',
     'authcart',
+    'whitenoise.runserver_nostatic'
+    
 ]
 
 MIDDLEWARE = [
@@ -53,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
